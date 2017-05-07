33b00678db6cf44cca685093ade7a48bbcc9e55f
pluszak <luszukpawel@gmail.com>
2017-05-08 01:20:17 +0200

    Working Build

---
+++ ARWorlds/Assets/Scripts/CreateAssetBundle.cs
@@ -0,0 +1,16 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateAssetBundle : MonoBehaviour
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles("Assets/StreamingAssets", BuildAssetBundleOptions.None,
+            BuildTarget.WSAPlayer);
+    }
+}
+#endif
---
+++ ARWorlds/Assets/Scripts/LoadAssetLocal.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class LoadAssetLocal : MonoBehaviour
+{
+    public List<string> assetBundleName = new List<string>();
+
+    IEnumerator Start()
+    {
+
+        var info = new DirectoryInfo(Application.streamingAssetsPath);
+        var fileInfo = info.GetFiles();
+
+        foreach (var file in fileInfo)
+        {
+            if(!(Path.GetFileNameWithoutExtension(file.ToString()).Contains(".manifest") || Path.GetFileNameWithoutExtension(file.ToString()).Contains("AssetBundles")))
+            {
+                Debug.Log(Path.GetFileNameWithoutExtension(file.ToString()));
+
+                if(! assetBundleName.Contains(Path.GetFileNameWithoutExtension(file.ToString())))
+                {
+                    assetBundleName.Add(Path.GetFileNameWithoutExtension(file.ToString()));
+                }
+                
+            }
+           
+        }
+
+        foreach (var name in assetBundleName)
+        {
+            var bundleLoadRequest =
+                AssetBundle.LoadFromFileAsync(Path.Combine(Application.streamingAssetsPath, name));
+
+
+            yield return bundleLoadRequest;
+
+            var myLoadAssetBundle = bundleLoadRequest.assetBundle;
+            if (myLoadAssetBundle == null)
+            {
+                Debug.Log("Failed to load Asset Bundle !");
+                yield break;
+            }
+
+            GameObject[] assetLoadRequest = myLoadAssetBundle.LoadAllAssets<GameObject>();
+            yield return assetLoadRequest;
+
+            foreach (GameObject gameObject in assetLoadRequest)
+            {
+                Instantiate(gameObject);
+                Debug.Log(gameObject.name + " Created");
+            }
+        }
+       
+
+    }
+
+}
